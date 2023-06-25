10ef664e6ed22c6714de70a0260c946251a39fa4
vladkasyan <voadkasyan.2006@gmail.com>
2023-06-25 23:37:44 +0300

    task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const collection = document.querySelector('.gallery');
+
+const listImg = galleryItems.map(({preview, original, description}) => 
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`).join('')
+
+collection.insertAdjacentHTML('afterbegin', listImg)
+
+collection.addEventListener("click", onOpenLargeImage)
+
+function onOpenLargeImage (event) {
+    event.preventDefault();
+    if (event.target.classList.contains("gallery__item")) {
+        return
+    }
+
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" alt="${event.target.description}" data-source="${event.target.dataset.source}" width="800" heigth="600">`,
+    {
+        onShow : () => {
+            document.addEventListener("keydown", onEsc)
+        },
+        onClose : () => {
+            document.removeEventListener("keydown", onEsc)
+        },
+    });
+    instance.show()
+
+    function onEsc(event) {
+        if(event.code === "Escape"){
+            instance.close();
+        }
+    }
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const collection = document.querySelector('.gallery');
+
+const listImg = galleryItems.map(({preview, original, description}) => 
+`<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`).join('')
+
+collection.insertAdjacentHTML('afterbegin', listImg)
+function onOpenLargeImage (event) {
+    event.preventDefault();
+    if (event.target.classList.contains("gallery__item")) {
+        return
+    }
+
+
+}
+
+const lightBox = new SimpleLightbox(collection, {
+    captionsData: `alt`,
+    captionsDelay: 250,
+})
+collection.spinner
+collection.addEventListener('click', onOpenLargeImage)
 
 console.log(galleryItems);
